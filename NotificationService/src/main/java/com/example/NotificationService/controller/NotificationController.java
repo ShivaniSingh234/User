a11292804b7c36d3f68bb635fad2a42a3c68cc6c
Shivani <shivusingh23456@gmail.com>
2025-10-30 17:44:13 +0530

    Initial

---
+++ NotificationService/src/main/java/com/example/NotificationService/controller/NotificationController.java
@@ -0,0 +1,17 @@
+package com.example.NotificationService.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/notify")
+public class NotificationController {
+
+	@GetMapping("/{user}")
+    public String sendNotification(@PathVariable Long userId) {
+        return "Notification sent to user: " + userId;
+    }
+}
